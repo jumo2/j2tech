05ac669353beafc21635f10d19544e7341a54e00
Judd Morgan <6760345+jumo2@users.noreply.github.com>
2020-09-25 09:36:24 -0500

    Added Navbar placeholder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Navigation from './Navigation'
 import './App.css';
 
-function App() {
+
+const App = () => (
+  <Container className="container-sm">
+    <Navigation />
+  </Container>
+);
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
-}
+} */
+
+
 
 export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+
+
+function Navigation() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">J2 Technology Solutions</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    );
+}
+
+
+
+export default Navigation;
