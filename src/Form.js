1cc31a09d67cfb25b2a28a7ce3a1a6bb182bd3e9
Judd Morgan <6760345+jumo2@users.noreply.github.com>
2020-10-03 11:46:44 -0500

    Continuing to modify UI Layout

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import './App.css'
+import { Toast } from 'react-bootstrap';
+
+
+function About() {
+    return (
+        <Toast>
+            
+        </Toast>    
+    )
+}
+
+export default About;
--- src/Navigation.js
+++ src/Navigation.js
@@ -6,16 +6,8 @@ import './App.css'
 
 function Navigation() {
     return (
-        <Navbar collapseOnSelect expand="lg">
-            <Navbar.Brand href="#home">J2 Technology Solutions</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+        <Navbar className="mx-auto fluid" collapseOnSelect expand="lg">
+            <Navbar.Brand className="brand mx-auto" href="#home">J2 Technology Solutions</Navbar.Brand>
         </Navbar>
     );
 }
